6b954ec7137081389499196d6518eb6e8170ea0f
Aaditya0018 <aadityashakle18@gmail.com>
2023-04-02 00:27:28 +0530

    first commit
    Basic Functionality added

---
+++ imdb/src/App.js
@@ -0,0 +1,24 @@
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import './App.css';
+import Header from "./components/header/Header";
+import Home from "./pages/home/Home";
+import MovieList from "./components/movieList/MovieList";
+import MovieDetail from "./pages/movieDetail/Movie"
+
+function App() {
+  return (
+    <div className='App'>
+      <Router>
+        <Header/>
+        <Routes>
+          <Route index element={<Home />}></Route>
+          <Route path="movie/:id" element={<MovieDetail />}> </Route>
+          <Route path="movies/:type" element={<MovieList />}></Route>
+          <Route path="/*" element={<h1>Error page</h1>}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
